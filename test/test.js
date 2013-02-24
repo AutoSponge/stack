19775eb17958148bc37e3852c84af32d2bbc8784
Paul Grenier <pgrenier@gmail.com>
2013-02-24 18:40:22 -0500

    .

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-test("Test", function() {
+test("Test basic stack and push with call", function() {
     function a(val) {
         return "a" + val;
     }
@@ -8,38 +8,33 @@ test("Test", function() {
     function c(val) {
         return "c" + val;
     }
-    expect(6);
+    expect(4);
     ok(Stack(a).call(1) === "a1");
     ok(Stack(a).push(b).call(1) === "ab1");
     ok(Stack(a).push(b).push(c).call(1) === "abc1");
-    ok(Stack([c,b,a]).call(1) === "abc1");
-    var stack = Stack(c);
-    stack.add(b).add(a);
-    ok(stack.call(1) === "abc1");
-    ok(Stack(c).addAll([b,a]).call(1) === "abc1");
+    ok(Stack([a,b,c]).call(1) === "abc1");
 });
 
-/*
-function flatten() {
-    var args = Array.prototype.slice.call(arguments, 0);
-	return args.reduce(function(a, b) {
-		return a.concat(b);
-	});
-}
-
-function doubleAll() {
-	var args = Array.prototype.slice.call(arguments, 0);
-	return args.map(function(a) {
-		return a + a;
-	});
-}
-
-function sum() {
-    var args = Array.prototype.slice.call(arguments, 0);
-    return args.reduce(function(a, b) {
-        return a + b;
-    });
-}
-
-Stack([sum, doubleAll, flatten]).apply([[1],[2],[3]]);
-*/
+test("Test basic stack and push with apply", function() {
+    function flatten() {
+        var args = Array.prototype.slice.call(arguments, 0);
+    	return args.reduce(function(a, b) {
+    		return a.concat(b);
+    	});
+    }
+    function doubleAll() {
+    	var args = Array.prototype.slice.call(arguments, 0);
+    	return args.map(function(a) {
+    		return a + a;
+    	});
+    }
+    function sum() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        return args.reduce(function(a, b) {
+            return a + b;
+        });
+    }
+    expect(2);
+    ok(Stack(sum).push(flatten).apply([[1],[2],[3]]) === 6);
+    ok(Stack([sum, doubleAll, flatten]).apply([[1],[2],[3]]) === 12);
+});
