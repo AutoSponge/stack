2c1136c9b407e8b79bb96fa4d98e5af1c383f950
Paul Grenier <pgrenier@gmail.com>
2013-02-27 14:52:29 -0500

    Update test/test.js
    pubsub idea

--- test/test.js
+++ test/test.js
@@ -38,3 +38,54 @@ test("Test basic stack and push with apply", function() {
     ok(Stack(sum).push(flatten).apply([[1],[2],[3]]) === 6);
     ok(Stack([sum, doubleAll, flatten]).apply([[1],[2],[3]]) === 12);
 });
+/*
+(function (namespace) {
+    var topics = {};
+    function getTopic(key) {
+        return topics[key];
+    }
+    function isStack(stack) {
+        return stack instanceof Stack;
+    }
+    function derivatives(topic) {
+        return str.split("/").map(function (e, i, arr) {
+            var copy = arr.slice(0);
+            return i === 0 ? str : (copy.splice(i, 1, "*"), copy.join("/"));
+        });
+    }
+    function getStacks(topic) {
+        return derivatives(topic).map(getTopic).filter(isStack);
+    }
+    function PubSub(id) {
+        if (!(this instanceof PubSub)) {
+            return new PubSub(id);
+        }
+        if (!PubSub.index) {
+            PubSub.index = {};
+        }
+        PubSub.index[id] = this;
+        this.id = id;
+    }
+    PubSub.prototype.subscribe = function (topic, fn) {
+        var stack = topics[topic];
+        if (!stack) {
+            topics[topic] = new Stack(fn);
+        } else {
+            stack.push(fn);
+        }
+        return this;
+    };
+    PubSub.prototype.unsubscribe = function (topic, fn) {
+        var stack = topics[topic];
+        
+        return this;
+    };
+    PubSub.prototype.publish = function (topic, data, context) {
+        getStacks(topic).forEach(function (stack) {
+            stack.call(context, data);
+        });
+    };
+    
+    namespace.PubSub = PubSub;
+}(this));
+*/
