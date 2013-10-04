b59ba1e3dccf5263b95c4fb4989a93f8804859e4
AutoSponge <pgrenier@gmail.com>
2013-10-04 11:40:00 -0400

    adding grunt and build tasks fixed test

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'features/step_definitions/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        qunit: {
+            all: ['src-test/*.html', 'example-test/*.html']
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.registerTask('default', ['jshint', 'qunit', 'uglify']);
+};
--- example-test/pubsub.test.js
+++ example-test/pubsub.test.js
@@ -1,3 +1,24 @@
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+        if (typeof this !== "function") {
+            // closest thing possible to the ECMAScript 5 internal IsCallable function
+            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        }
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function () {},
+            fBound = function () {
+                return fToBind.apply(this instanceof fNOP && oThis
+                    ? this
+                    : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+        return fBound;
+    };
+}
+
 module("PubSub");
 test("test PubSub has the correct api", function () {
     expect(5);
@@ -39,7 +60,7 @@ test("test PubSub can bind a handler to a receiver", function () {
         this.called += 1;
     }, heard);
     PubSub.publish("test/bind")();
-    ok(heard.called = 1);
+    ok(heard.called === 1);
 });
 test("test PubSub can unsubscribe", function () {
     var heard = null;
